b5eb6b14618ccf39e1ebcf2884e6eaa9ec45f793
Tomáš Kelbasa <tomas.plqa@gmail.com>
2023-05-22 16:45:00 +0200

    minor changes

--- FalloutMinigame/FalloutMinigame/Objects/SaveSystem.cs
+++ FalloutMinigame/FalloutMinigame/Objects/SaveSystem.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace FalloutMinigame.Objects
 {
+    /// <summary>
+    ///     Třída sloužící pro správu savů.
+    /// </summary>
     internal class SaveSystem
     {
+
+        public int SavePlayer(Player p)
+        {
+            string filePath = "Cesta_k_XML_souboru.xml";
+
+            StreamWriter xmlWriter = new StreamWriter("./Resource/Saves/" + p.Name + ".xml");
+
+            
+
+            return 0;
+        }
+
     }
 }
